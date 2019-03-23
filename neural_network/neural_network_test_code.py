f79cae0bc5888f0bb036709d232de6e323ed9ad6
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2019-03-23 13:45:06 -0400

    Update neural_network_test_code.py

--- neural_network/neural_network_test_code.py
+++ neural_network/neural_network_test_code.py
@@ -2,5 +2,8 @@ import neural_network as n
 
 def main():
 	nn = n.NeuralNetwork([3, 4, 5, 4])
-	print("test")
+	print("Weights:", nn.get_weights())
+	print("Values:", nn.get_values())
+	nn.re_value([.8, .2, .1, .9])
+	print("Values:", nn.get_values())
 main()
