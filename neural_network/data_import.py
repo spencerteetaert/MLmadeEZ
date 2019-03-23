112f232db9693d2e5594bdc3ab6a7b299928206c
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2019-03-23 18:01:16 -0400

    Update data_import.py

--- neural_network/data_import.py
+++ neural_network/data_import.py
@@ -1,7 +1,23 @@
-def import_weights(self, address):
-	file = open(address, "r")
-	count = 0
-	values = file.read().split(',')
+def import_data():
+	file = open('iris.data.txt', "r")
+	values = file.read().split('\n')
 	ret = []
+	
+	outputs = {}
+	count = 0
 
-	print(values)
+	for j in range(0, len(values), 1):
+		inputElem = []
+		rowElem = values[j].split(',')
+		for i in range(0, len(rowElem) - 1, 1):
+			inputElem += [float(rowElem[i])]
+
+			if (i == len(rowElem)-2):
+				if not (rowElem[i+1] in outputs):
+					outputs[rowElem[i+1]] = count 
+					count = count + 1
+
+		ret += [inputElem + [count-1]]
+
+	return [ret] + [outputs]
+	#print(values)
