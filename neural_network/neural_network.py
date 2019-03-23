75e6ab2c728f82bd02b7f5944d14f32fe39a4a8c
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2019-03-23 12:21:17 -0400

    Update neural_network.py

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -27,11 +27,13 @@ class NeuralNetwork:
 
 				nf_weightToAdd = []
 				if (i + 1 < self.layers):
+					#If the next layer exists, generate weights to
 					for k in range(0, initConditions[i + 2] + 1):
 						ranWeight = random.randint(-100, 100)/100
 						nf_weightToAdd += [ranWeight]
 					lf_weightToAdd += [nf_weightToAdd]
 				else:
+					#If this is last layer, add empty (for indexing)
 					for k in range(0, initConditions[i + 1] + 1):
 						nf_weightToAdd += []
 					lf_weightToAdd += [nf_weightToAdd]
@@ -40,6 +42,7 @@ class NeuralNetwork:
 				self.weights[0] += [lf_weightToAdd]
 			self.values += [l_valToAdd]
 
+		#To allow for back-prop, create the second half of the array to allow for easy reverse indexing
 		self.create_back_path(initConditions)
 
 	def create_back_path(self, initConditions):
@@ -50,6 +53,7 @@ class NeuralNetwork:
 				nb_weightToAdd = []
 				if (i >= 1):
 					for k in range(0, initConditions[i] + 1):
+						#Fetches
 						nb_weightToAdd += [self.weights[0][i-1][k][j]]
 				else:
 					for k in range(0, initConditions[i + 1] + 1):
@@ -85,3 +89,39 @@ class NeuralNetwork:
 		for i in range(0, len(u), 1):
 			cost += (u[i] - self.values[self.layers - 1][i])**2
 		return cost
+
+	#####################
+	### Gets and Sets ###
+	#####################
+
+	def get_weights(self):
+		return self.weights
+	def get_values(self):
+		return self.values
+
+	##########################
+	### Data export/import ###
+	##########################
+
+	def import_weights(self, address):
+		file = open(address, "r")
+		count = 0
+		values = file.read().split(',')
+		for i in range(0, 2, 1):
+			for j in range(0, len(self.weights[i]), 1):
+				for k in range(0, len(self.weights[i][j]), 1):
+					for q in range(0, len(self.weights[i][j][k]), 1):
+						self.weights[i][j][k][q] = float(values[count])
+						count = count + 1
+		return True
+
+	def export_weights(self, address):
+		file = open(address, "w+")
+
+		for i in range(0, 2, 1):
+			for j in range(0, len(self.weights[i]), 1):
+				for k in range(0, len(self.weights[i][j]), 1):
+					for q in range(0, len(self.weights[i][j][k]), 1):
+						file.write(str(self.weights[i][j][k][q]) + ',')
+
+		return True
