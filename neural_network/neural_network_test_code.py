aab930c6c2f89968eae9f2df4b4d725357883e88
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2019-03-23 16:30:37 -0400

    Update neural_network_test_code.py

--- neural_network/neural_network_test_code.py
+++ neural_network/neural_network_test_code.py
@@ -4,6 +4,6 @@ def main():
 	nn = n.NeuralNetwork([3, 4, 5, 4])
 	print("Weights:", nn.get_weights())
 	print("Values:", nn.get_values())
-	nn.re_value([.8, .2, .1, .9])
+	nn.forward_propagate([.8, .2, .1, .9])
 	print("Values:", nn.get_values())
 main()
