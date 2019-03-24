b73149d81fd97b05fbb2483220d4620963efb00e
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2019-03-23 20:01:07 -0400

    Update neural_network.py

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -1,137 +1,145 @@
-import random
-import math
-import numpy
-import scikit
+# Code taken from Jason Brownlee's neural network tutorial
+# https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/ 
 
-class NeuralNetwork:
-	######################
-	### Initialization ###
-	######################
+# Altered by MLmadeEZ, an IEEE NewHacks team
+from math import exp
+from random import seed
+from random import random
+from random import sample
+from random import randint
+from data_import import import_data
+ 
+# Initialize a network
+def initialize_network(n_inputs, n_hidden, n_outputs):
+	network = list()
+	hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+	network.append(hidden_layer)
+	output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+	network.append(output_layer)
+	return network
+ 
+# Calculate neuron activation for an input
+def activate(weights, inputs):
+	activation = weights[-1]
+	for i in range(len(weights)-1):
+		activation += weights[i] * inputs[i]
+	return activation
+ 
+# Transfer neuron activation
+def transfer(activation):
+	return 1.0 / (1.0 + exp(-activation))
+ 
+# Forward propagate input to a network output
+def forward_propagate(network, row):
+	inputs = row
+	for layer in network:
+		new_inputs = []
+		for neuron in layer:
+			activation = activate(neuron['weights'], inputs)
+			neuron['output'] = transfer(activation)
+			new_inputs.append(neuron['output'])
+		inputs = new_inputs
+	return inputs
+ 
+# Calculate the derivative of an neuron output
+def transfer_derivative(output):
+	return output * (1.0 - output)
+ 
+# Backpropagate error and store in neurons
+def backward_propagate_error(network, expected):
+	for i in reversed(range(len(network))):
+		layer = network[i]
+		errors = list()
+		if i != len(network)-1:
+			for j in range(len(layer)):
+				error = 0.0
+				for neuron in network[i + 1]:
+					error += (neuron['weights'][j] * neuron['delta'])
+				errors.append(error)
+		else:
+			for j in range(len(layer)):
+				neuron = layer[j]
+				errors.append(expected[j] - neuron['output'])
+		for j in range(len(layer)):
+			neuron = layer[j]
+			neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+ 
+# Update network weights with error
+def update_weights(network, row, l_rate):
+	for i in range(len(network)):
+		inputs = row[:-1]
+		if i != 0:
+			inputs = [neuron['output'] for neuron in network[i - 1]]
+		for neuron in network[i]:
+			for j in range(len(inputs)):
+				neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+			neuron['weights'][-1] += l_rate * neuron['delta']
+ 
+# Train a network for a fixed number of epochs
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+	for epoch in range(n_epoch):
+		sum_error = 0
+		for row in train:
+			outputs = forward_propagate(network, row)
+			expected = [0 for i in range(n_outputs)]
+			expected[row[-1]] = 1
+			sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
+			backward_propagate_error(network, expected)
+			update_weights(network, row, l_rate)
+		#print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+ 
+# Test training backprop algorithm
+print("Network is training...")
+seed(1)
+dataset = import_data()
+dataset = sample(dataset, len(dataset))
+n_inputs = len(dataset[0][0]) - 1
+n_outputs = len(set([row[-1] for row in dataset[0]]))
+network = initialize_network(n_inputs, 5, n_outputs)
+train_network(network, dataset[0], 0.1, 100, n_outputs)
 
-	def __init__(self, initConditions):
-		# initConditions = [ numLayers, numNodes, nuNodes, ... ]
-		self.layers = initConditions[0]
-		self.weights = [[],[]] # 0: forward weights 1: backward weights
-		self.values = []
+# Step size
+# network size
 
-		#For each layer
-		for layer in range(0, self.layers, 1): 
-			l_valToAdd = []
-			lf_weightToAdd = []
-			#For each node + 1 bias 
-			for node in range(0, initConditions[layer + 1] + 1):
-				if (node < initConditions[layer + 1]):
-					#Adding 0's to every node except bias
-					l_valToAdd += [0]
-				else:
-					l_valToAdd += [1]
+count = 0
+for i in range(0, len(dataset[0]), 1):		
+	outputs = forward_propagate(network, dataset[0][i])
+	current = [0, 0]
+	for i in range(0, len(outputs), 1):
+		if outputs[i] > current[0]:
+			current = [outputs[i], i]
+	print(dataset[1][dataset[0][i][4]], dataset[1][current[1]])
+	if (dataset[1][dataset[0][i][4]] == dataset[1][current[1]]):
+		count = count + 1
+print(count/len(dataset[0])*100, "% sucess rate")
 
-				nf_weightToAdd = []
-				if (layer + 1 < self.layers):
-					#If the next layer exists, generate weights to
-					for nextNode in range(0, initConditions[layer + 2] + 1):
-						ranWeight = random.randint(-100, 100)/100
-						nf_weightToAdd += [ranWeight]
-					lf_weightToAdd += [nf_weightToAdd]
-				else:
-					#If this is last layer, add empty (for indexing)
-					for nextNode in range(0, initConditions[layer + 1] + 1):
-						nf_weightToAdd += []
-					lf_weightToAdd += [nf_weightToAdd]
+# while True:
+# 	rand = randint(0, 149)
+# 	input("...")
 
-			if (len(lf_weightToAdd) > 0):
-				self.weights[0] += [lf_weightToAdd]
-			self.values += [l_valToAdd]
+# 	outputs = forward_propagate(network, dataset[0][rand])
+# 	current = [0, 0]
+# 	for i in range(0, len(outputs), 1):
+# 		if outputs[i] > current[0]:
+# 			current = [outputs[i], i]
+# 	print("At data point", current[1], "desired answer:", dataset[1][dataset[0][rand][4]])
+# 	print("Prediction:", dataset[1][current[1]], "with", round(current[0]*100, 1), "% certainty")
+# 	if (dataset[1][dataset[0][rand][4]] == dataset[1][current[1]]):
+# 		print("#Success")
+# 	else:
+# 		print("#Fail")
 
-		#To allow for back-prop, create the second half of the array to allow for easy reverse indexing
-		self.create_back_path(initConditions)
+# while True:
+# 	inputString = input("Enter test case:\n")
+# 	temp = inputString.split(',')
+# 	inputList = []
+# 	for i in range(0, len(temp), 1):
+# 		inputList += [float(temp[i])]
 
-	def create_back_path(self, initConditions):
-		for layer in range(0, self.layers, 1):
-			lb_weightToAdd = []
+# 	outputs = forward_propagate(network, inputList)
+# 	current = [0, 0]
+# 	for i in range(0, len(outputs), 1):
+# 		if outputs[i] > current[0]:
+# 			current = [outputs[i], i]
 
-			for nextNode in range(0, initConditions[layer + 1] + 1):
-				nb_weightToAdd = []
-				if (layer >= 1):
-					for node in range(0, initConditions[layer] + 1):
-						#Fetches
-						nb_weightToAdd += [self.weights[0][layer-1][node][nextNode]]
-				else:
-					for node in range(0, initConditions[layer + 1] + 1):
-						nb_weightToAdd += []
-				lb_weightToAdd += [nb_weightToAdd]
-
-			if (len(lb_weightToAdd) > 0):
-				self.weights[1] += [lb_weightToAdd]
-
-		return True
-
-	##################
-	### Operations ###
-	##################
-
-	def sigmoid(self, x):
-		return (1/(1+math.exp(-1*x)))
-
-	def forward_propagate(self, x):
-		for i in range(0, len(x), 1):
-			self.values[0][i] = x[i]
-
-		for layer in range(1, self.layers, 1):
-			for node in range(0, len(self.weights[1][layer]), 1):
-				accum = 0
-				for pastNode in range(0, len(self.weights[1][layer-1]), 1):
-					accum += self.values[layer-1][pastNode] * self.weights[1][layer][node][pastNode]
-				self.values[layer][node] = self.sigmoid(accum)
-		return True
-
-	def get_cost(self, expected):
-		cost = 0
-		output = self.values[self.layers - 1]
-		for i in range(0, len(expected), 1):
-			cost += (expected[i] - output[i])*transfer_derivative(output[i])
-		return cost
-
-	def transfer_derivative(self, output):
-		return output * (1 - output)
-
-	def back_propagate_error(self, x):
-		#Start w output, calc error ()
-		for layer in range(len(self.weights[0]), -1, -1):
-			return True
-
-	#####################
-	### Gets and Sets ###
-	#####################
-
-	def get_weights(self):
-		return self.weights
-	def get_values(self):
-		return self.values
-
-	##########################
-	### Data export/import ###
-	##########################
-
-	def import_weights(self, address):
-		file = open(address, "r")
-		count = 0
-		values = file.read().split(',')
-		for i in range(0, 2, 1):
-			for j in range(0, len(self.weights[i]), 1):
-				for k in range(0, len(self.weights[i][j]), 1):
-					for q in range(0, len(self.weights[i][j][k]), 1):
-						self.weights[i][j][k][q] = float(values[count])
-						count = count + 1
-		return True
-
-	def export_weights(self, address):
-		file = open(address, "w+")
-
-		for i in range(0, 2, 1):
-			for j in range(0, len(self.weights[i]), 1):
-				for k in range(0, len(self.weights[i][j]), 1):
-					for q in range(0, len(self.weights[i][j][k]), 1):
-						file.write(str(self.weights[i][j][k][q]) + ',')
-
-		return True
+# 	print("Prediction:", dataset[1][current[1]], "with", round(current[0]*100, 1), "% certainty\n")
