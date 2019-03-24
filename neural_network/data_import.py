f41c4050f4d20026dc4f03579f1c833e19578c1a
Divy <divyr@outlook.com>
2019-03-24 00:41:11 -0400

    merged

--- neural_network/data_import.py
+++ neural_network/data_import.py
@@ -6,30 +6,23 @@ import numpy
 def import_data(address, start):
 	file = open(address, "r")
 	values = file.read().split('\n')
-	ret = []
-	
-	outputs = {}
-	count = 0
-
+	ret = []  # list of all elements 
+	outputs = {}  # output dict
+	count = 0  # tracking output nodes
 	if(start == True): #Returns 100 random pieces of data
 		for i in range(0, 100, 1):
 			inputElem = []
 			rowElem = values[randint(0, len(values)-1)].split(',')
 			for j in range(0, len(rowElem) - 1, 1):
-				if (j == len(rowElem)-2):
-					if not (rowElem[j+1] in outputs.values()):
-						outputs[count] = rowElem[j+1]
-						#print("new output found:",outputs[count])
-						count = count + 1
+				if (j == len(rowElem)-2):  # 2nd last element, allows you to index the last element
+					if not (rowElem[j+1] in outputs.values()):  # last element (output) is already found
+						outputs[count] = rowElem[j+1]  # add undiscovered output
+						count = count + 1  # increment output counter
 				try:
-					inputElem += [float(rowElem[j])]
-					#print("input: #",inputElem[-1])
+					inputElem += [float(rowElem[j])]  # try to cast to float if possible
 				except:
-					#print("none float found")
 					continue
-
-			ret += [inputElem + [count-1]]
-			print(ret[-1])
+			ret += [inputElem]  
 		return [ret] + [outputs]
 
 	#biases = find_max_min(values)
