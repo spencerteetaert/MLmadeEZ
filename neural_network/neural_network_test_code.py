f98b9024ae205977215c59d1b0e0c820a4f544fb
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2019-03-23 20:01:01 -0400

    Delete neural_network_test_code.py

--- neural_network/neural_network_test_code.py
+++
@@ -1,9 +0,0 @@
-import neural_network as n
-
-def main():
-	nn = n.NeuralNetwork([3, 4, 5, 4])
-	print("Weights:", nn.get_weights())
-	print("Values:", nn.get_values())
-	nn.forward_propagate([.8, .2, .1, .9])
-	print("Values:", nn.get_values())
-main()
