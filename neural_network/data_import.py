58d80d82524c11b015c8e9fa779ceeb77b20e187
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2019-03-24 01:13:26 -0400

    ...

--- neural_network/data_import.py
+++ neural_network/data_import.py
@@ -2,38 +2,20 @@ import string
 from math import exp
 from random import randint
 import numpy
+import random
 
-def import_data(address, start):
+def import_data(address):
 	file = open(address, "r")
 	values = file.read().split('\n')
+	#values.shuffle()
+	#print(values)
 
 	ret = []  # list of all elements 
 	outputs = {}  # output dict
 	count = 0  # tracking output nodes
 
-	if(start == -1): #Returns 100 random pieces of data
-		for i in range(0, 100, 1):
-			inputElem = []
-			rowElem = values[randint(0, len(values)-1)].split(',')
-			for j in range(0, len(rowElem) - 1, 1):
-				if (j == len(rowElem)-2):  # 2nd last element, allows you to index the last element
-					if not (rowElem[j+1] in outputs.values()):  # last element (output) is already found
-						outputs[count] = rowElem[j+1]  # add undiscovered output
-						count = count + 1  # increment output counter
-				try:
-					inputElem += [float(rowElem[j])]  # try to cast to float if possible
-				except:
-					continue
-
-			ret += [inputElem + [list(outputs.values()).index(rowElem[len(rowElem) - 1])]]
-
-		print([ret] + [outputs])
-		return [ret] + [outputs]
-
-	#biases = find_max_min(values)
-
 	# Returns first 50 pieces of consecutive data
-	for i in range(min(start*50, len(values) - 50), min(start*50 + 50, len(values)), 1):
+	for i in range(0, len(values), 1):
 		inputElem = []
 		rowElem = values[i].split(',')
 		for j in range(0, len(rowElem) - 1, 1):
